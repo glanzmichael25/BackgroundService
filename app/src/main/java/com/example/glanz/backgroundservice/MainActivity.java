719cd3b2d9c34a45e0e5a8b714b2fd276d568b18
glanzmichael25 <glanz.michael.25@gmail.com>
2018-05-19 13:03:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/glanz/backgroundservice/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.glanz.backgroundservice;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btnPlay = (Button)findViewById(R.id.playButton);
+        final TextView textView = (TextView)findViewById(R.id.textView);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnPlay.getText().equals("Start")) {
+                    Intent myService = new Intent(MainActivity.this, MusicPlayer.class);
+                    startService(myService);
+                    btnPlay.setText("Stop");
+                    textView.setTextColor(Color.parseColor("#FF0000"));
+                }else {
+                    Intent myService = new Intent(MainActivity.this, MusicPlayer.class);
+                    stopService(myService);
+                    btnPlay.setText("Start");
+                    textView.setTextColor(Color.parseColor("#0000FF"));
+                }
+            }
+        });
+    }
+}
